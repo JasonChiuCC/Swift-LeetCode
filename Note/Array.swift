8963122bf5cce986845f878f0cc6d423bf30d547
jason.cc.chiu <jason.cc.chiu@delta.com.tw>
2016-03-03 23:20:15 +0800

    Modify Array.swift

--- Note/Array.swift
+++ Note/Array.swift
@@ -1,6 +1,9 @@
 # 初始化陣列
 let array = [Int](count: 300, repeatedValue: -1)
 
+# 陣列長度
+array.count
+
 # 將字串轉為字串陣列
 e.g. Hello -> ["H","e","l","l","o"]
 (1)
@@ -19,5 +22,13 @@ let b = Array(bStr.unicodeScalars)
 var myString = "Hello"
 let characters = [Character](myString.characters)
 
+# 將字串陣列轉字串
+var sb      = [String]()
+let strTest = sb.joinWithSeparator("")
+
 # 排序
 Array.sort(<)
+
+# 陣列加入元素
+var sb = [String]()
+sb.append("$")
