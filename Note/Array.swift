f182c7f706a1a14acea96f439bc43a01cab58a90
jason.cc.chiu <jason.cc.chiu@delta.com.tw>
2016-03-10 22:12:14 +0800

    Modify Node

--- Note/Array.swift
+++ Note/Array.swift
@@ -1,40 +1,42 @@
 # 初始化陣列
-let array = [Int](count: 300, repeatedValue: -1)
+let array           = [Int](count: 300, repeatedValue: -1)
+let array           = [String]()
+let array:[Int]     = [1, 2, 3]
+let array:[Int]     = []
+let array:[Int]     = [1, 2]
+
+# 初始化 Optional 陣列
+let array: Int?[]       = [1, 2, nil, 4, 5]
+let array: [Int?]       = [1, 2, nil, 4, 5]
+let array: Array<Int?>  = [1, 2, nil, 4, 5]
+let array               = [String?](count:64, repeatedValue: nil)
 
 # 陣列長度
 array.count
 
 # 將字串轉為字串陣列
 e.g. Hello -> ["H","e","l","l","o"]
-(1)
-let a = Array(aStr)
-let b = Array(bStr)
+// 轉陣列但元素為 Character
+let array = Array(aStr.characters)
+let array = Array(aStr.utf16)
+let array = Array(aStr.unicodeScalars)
 
-(2)
-let a = Array(aStr.utf16)
-let b = Array(bStr.utf16)
-
-(3)
-let a = Array(aStr.unicodeScalars)
-let b = Array(bStr.unicodeScalars)
+// 轉陣列且元素為 String
+aStr.characters.map { String($0) }
 
 # 將字串轉為字元陣列
-var myString = "Hello"
-let characters = [Character](myString.characters)
-let characters = Array(myString.characters)
+let myString    = "Hello"
+let characters  = [Character](myString.characters)
+let characters  = Array(myString.characters)
 
 # 將字串陣列轉字串
-var sb      = [String]()
-let strTest = sb.joinWithSeparator("")
+let strTest = array.joinWithSeparator("")
 
 # 排序
 Array.sort(<)
 
 # 陣列加入元素
-var sb = [String]()
-sb.append("$")
+array.append("$")
 
-# Optional 陣列
-var arr: Int?[] = [1, 2, nil, 4, 5]
-var arr: [Int?] = [1, 2, nil, 4, 5]
-var arr: Array<Int?> = [1, 2, nil, 4, 5]
+# 陣列移除元素
+strArray.removeAtIndex(0) 
--- Note/String.swift
+++ Note/String.swift
@@ -5,3 +5,38 @@ let inputStrLen = s.characters.count
 # 根據索引(idx)取字串字元 - 很慢
 let strIdx  = s.startIndex.advancedBy(idx)
 let nowChar = s.characters[strIdx]
+
+# 擷取字串
+Swift 1.2 用法
+位置(-)        9876543210
+位置           0123456789    
+var s       = "1234567890"
+let idx     = s.startIndex.advancedBy(5)  
+let idx2    = s.endIndex.advancedBy(-6)   
+var range   = Range<String.Index>(start: idx2,end: idx)
+
+var s1:String   = s.substringFromIndex(index)  // 由(From)字串第 5  位開始,包含第 5 位一直到最後  = [5...9]
+var s2:String   = s.substringToIndex(index2)   // 由開頭到(To)字串第 -6 位開始,包含第 -6 位       = [0...3]
+var s3          = s.substringWithRange(range)
+
+print(s1) // 67890
+print(s2) // 1234
+print(s3) // 5
+
+Swift 2.2 用法
+位置(-)        9876543210
+位置           0123456789
+var s       = "1234567890"   
+s[s.startIndex]                 // 1 取出字串中第 1 位
+s[s.endIndex.predecessor()]     // 0 取出字串中最後 1 位
+s[s.startIndex.successor()]     // 2 取出字串中最後 1 位的後面那一個
+
+let index = s.startIndex.advancedBy(7)
+s[index]  // 8 取出字串中第 7 位
+
+// 下面兩種範圍是一樣的意思
+let idx1 = s.startIndex.advancedBy(0)..<s.endIndex.advancedBy(-4)
+let idx2 = s.startIndex.advancedBy(0)..<s.startIndex.advancedBy(6)
+s[idx1]  // 123456  
+s[idx2]  // 123456
+
