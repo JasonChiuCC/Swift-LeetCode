aa717f6aa5ee19409169a8f7047605921f69be26
jason.cc.chiu <jason.cc.chiu@delta.com.tw>
2016-03-12 01:26:49 +0800

    Modify Note

--- Note/For.swift
+++ Note/For.swift
@@ -10,3 +10,8 @@ for idx in 0..<10 {
 for idx in 10.stride(to: 0, by: -1) {
     print("idx = \(idx)")
 } 
+
+# 使用 Map 遞迴序列
+var a1  = [1,2,3,4,5,6]
+var a1m = a1.map({$0 * 2})
+print(a1m) // [2, 4, 6, 8, 10, 12]
